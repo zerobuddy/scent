62176056af456a7ab1cf7f43d32198c2830fdcf5
zerobuddy <09bit097@gmail.com>
2020-05-26 02:16:01 +0530

    Retrofit library addition

---
+++ app/src/main/java/com/scent/business/ApiClient.java
@@ -0,0 +1,15 @@
+package com.scent.business;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    private static String BASE_URL = "";
+    public static Retrofit retrofit;
+    public static Retrofit getApiClient(){
+      if(retrofit != null){
+          retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+      }
+      return retrofit;
+    }
+}
